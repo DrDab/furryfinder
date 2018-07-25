32a694d90229d6a61170e4b3f37ea048eac32671
_c0da_ <duvictor514@gmail.com>
2018-07-25 12:42:25 -0700

    Added settings activity and clickable list items

--- app/src/main/java/com/openhorizonsolutions/findyourfurry/FurryList.java
+++ app/src/main/java/com/openhorizonsolutions/findyourfurry/FurryList.java
@@ -1,9 +1,17 @@
 package com.openhorizonsolutions.findyourfurry;
 
+import android.provider.ContactsContract;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.text.DecimalFormat;
+
+import furrylib.Furry;
 
 public class FurryList extends AppCompatActivity
 {
@@ -24,5 +32,37 @@ public class FurryList extends AppCompatActivity
                         DataStore.withinRangeString);
 
         contestList.setAdapter(adapter);
+
+        contestList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView arg0, View arg1, int position, long arg3)
+            {
+                // position = position selected
+                AlertDialog alertDialog = new AlertDialog.Builder(FurryList.this).create();
+                alertDialog.setTitle("Furry Information");
+                if (DataStore.withinRange.size() >= 1)
+                {
+                    Furry furry = DataStore.withinRange.get(position);
+                    String s = "";
+                    s += "ID: " + furry.getID() + "\n";
+                    s += "Username: " + furry.getUserName() + "\n";
+                    s += "Description: " + furry.getDescription() + "\n";
+                    s += "Profile: http://furrymap.net" + furry.getProfile() + "\n";
+                    s += "Distance: " + new DecimalFormat("#.##").format(furry.distanceFromCoords(DataStore.latitude, DataStore.longitude)) + " miles\n";
+                    s += "Latitude: " + furry.getLatitude() + "°\n";
+                    s += "Longitude: " + furry.getLongitude() + "°\n";
+                    alertDialog.setMessage(s);
+                    alertDialog.show();
+                }
+                else
+                {
+                    alertDialog.setMessage("No Furries Yet");
+                    alertDialog.show();
+                }
+
+            }
+        });
+
     }
 }
