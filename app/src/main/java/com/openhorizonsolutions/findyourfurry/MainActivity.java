5c8df0baecbe713285e6a092bdda8711d95c33eb
DrDab <duvictor514@gmail.com>
2018-12-24 12:11:28 -0800

    Added don't show again message to alert dialog.

--- app/src/main/java/com/openhorizonsolutions/findyourfurry/MainActivity.java
+++ app/src/main/java/com/openhorizonsolutions/findyourfurry/MainActivity.java
@@ -4,16 +4,20 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.os.Environment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -58,15 +62,37 @@ public class MainActivity extends AppCompatActivity
             String carrier = tm.getNetworkOperatorName();
             if (carrier != null)
             {
-                if (carrier.toLowerCase().indexOf("verizon") != -1)
+                File readDirectory = new File(Environment.getExternalStorageDirectory(), "FindYourFurry");
+                if (!readDirectory.exists())
                 {
-                    final AlertDialog alertDialog1 = new AlertDialog.Builder(MainActivity.this).create();
+                    readDirectory.mkdir();
+                }
+                final File notice = new File(readDirectory, "notice");
+                if (carrier.toLowerCase().indexOf("verizon") == -1 && !notice.exists())
+                {
+                    AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+                    LayoutInflater adbInflater = LayoutInflater.from(this);
+                    View eulaLayout = adbInflater.inflate(R.layout.checkbox, null);
+                    final CheckBox dontShowAgain = (CheckBox) eulaLayout.findViewById(R.id.skip);
+                    adb.setView(eulaLayout);
+                    final AlertDialog alertDialog1 = adb.create();
                     alertDialog1.setTitle("Verizon User - Please Read!");
-                    alertDialog1.setMessage("Verizon is a carrier that supports the censorship of free information and violation of Internet freedoms, such as the right to private communications and neutral speeds, for its' own gain. It is strongly advised that you switch to a carrier that cares about your freedoms. Thank you for using my software! ;-)");
+                    alertDialog1.setMessage("Verizon is a carrier that supports the censorship of free information and violation of Internet freedoms, such as the right to private communications and neutral speeds, for its' own financial gain. It is strongly advised that you switch to a carrier that respects your electronic freedoms. Thank you for using my software! ;-)");
                     alertDialog1.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener()
                     {
                         public void onClick(DialogInterface dialog, int id)
                         {
+                            if (dontShowAgain.isChecked())
+                            {
+                                try
+                                {
+                                    notice.createNewFile();
+                                }
+                                catch (IOException e)
+                                {
+                                    e.printStackTrace();
+                                }
+                            }
                             alertDialog1.dismiss();
                         }
                     });
